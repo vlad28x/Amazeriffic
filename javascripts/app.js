6de564de72c745c0f93ae118d6810d7b32362886
vlad28x <vlad.skibin@mail.ru>
2021-04-24 21:49:53 +0600

    Implementation a tab handler using a forEach

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,12 @@
 var main = function() {
 	"use strict";
-	for(var tabNumber = 1; tabNumber < 4; ++tabNumber) {
-		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		$(tabSelector).on("click", function() {
-			$(".tabs span").removeClass("active");
-			$(this).addClass("active");
+	$(".tabs a span").toArray().forEach(function(element) {
+		$(element).on("click", function() {
+			$(".tabs a span").removeClass("active");
+			$(element).addClass("active");
 			$("main .content").empty();
 			return false;
-		});
-	}
+		})
+	});
 }
 $(document).ready(main);
